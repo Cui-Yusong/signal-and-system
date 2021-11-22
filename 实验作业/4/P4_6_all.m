ffae660b697c40d9a29d16b41619ac88880132dd
AlpAcA0072@gmail.com <AlpAcA0072@gmail.com>
2021-11-22 21:53:11 +0800

    lab4 assgn

--- 实验作业/4/P4_6_all.m
+++ 实验作业/4/P4_6_all.m
@@ -1,99 +1,78 @@
 load ctftmod.mat
 
-% 4.6(a)
+% 4.6_a
 z = [dash dash dot dot];
 
-% 4.6(b)
+
+% 4.6_b
 figure;
 freqs(bf, af)
 title('Frequency response of bf and af');
-saveas(gcf, "plots/P4_6_b_out.png")
+saveas(gcf, "./P4_6_b.png")
 close
 
-% 4.6(c)
+% 4.6_c
 
-ydash = lsim(bf, af, dash, t(1:length(dash)))
-ydot = lsim(bf, af, dot, t(1:length(dot)))
+ydash = lsim(bf, af, dash, t(1:length(dash)));
+ydot = lsim(bf, af, dot, t(1:length(dot)));
 
 figure;
 subplot(2,1,1)
 hold on;
 plot(t(1:length(dash)), dash);
 plot(t(1:length(dash)), ydash);
-legend('dash', 'dash after lowpass filter', 'Location', 'northeast');
-title('dash and dash after lowpass filter');
+legend('dash without lowpass filter', 'dash with lowpass filter', 'Location', 'northeast');
+title('Dash');
 xlabel('t');
 subplot(2,1,2)
 hold on;
 plot(t(1:length(dot)), dot);
 plot(t(1:length(dot)), ydot);
-title('dot and dot after lowpass filter');
-legend('dot', 'dot after lowpass filter', 'Location', 'northeast');
+legend('dot without lowpass filter', 'dot with lowpass filter', 'Location', 'northeast');
+title('Dot');
 xlabel('t');
-saveas(gcf, 'plots/P4_6_c_out.png')
+saveas(gcf, './P4_6_c.png')
 close
 
-% 4.6(d)
-y = dash .* cos(2*pi*f1*t(1:length(dash)))
-yo = lsim(bf, af, y, t(1:length(y)))
+% 4.6_d
+y = dash .* cos(2*pi*f1*t(1:length(dash)));
+yo = lsim(bf, af, y, t(1:length(y)));
 
 figure;
 subplot(2,1,1)
 hold on;
 plot(t(1:length(dash)), y);
-title('y');
+title('Without filter');
 subplot(2,1,2)
 plot(t(1:length(y)), yo);
-title('After being filtered');
+title('With filter');
 
-saveas(gcf, 'plots/P4_6_d_out.png')
+saveas(gcf, './P4_6_d.png')
 close
 
-% 4.6(e)
+% 4.6_g
+x1 = x .* cos(2*pi*f1*t(1:length(x)));
+m1 = lsim(bf, af, x1, t(1:length(x1)));
 
-% 4.6(f)
+x2 = x .* sin(2*pi*f2*t(1:length(x)));
+m2 = lsim(bf, af, x2, t(1:length(x2)));
 
-% 4.6(g)
-% x1 = x .* sin(2*pi*f1*t(1:length(x)))
-% m1 = lsim(bf, af, x1, t(1:length(x1)))
-
-% x2 = x .* sin(2*pi*f2*t(1:length(x)))
-% m2 = lsim(bf, af, x2, t(1:length(x2)))
-
-% x3 = x .* cos(2*pi*f1*t(1:length(x)))
-% m3 = lsim(bf, af, x3, t(1:length(x3)))
-
-x1 = x .* cos(2*pi*f1*t(1:length(x)))
-m1 = lsim(bf, af, x1, t(1:length(x1)))
-
-x2 = x .* sin(2*pi*f2*t(1:length(x)))
-m2 = lsim(bf, af, x2, t(1:length(x2)))
-
-x3 = x .* sin(2*pi*f1*t(1:length(x)))
-m3 = lsim(bf, af, x3, t(1:length(x3)))
+x3 = x .* sin(2*pi*f1*t(1:length(x)));
+m3 = lsim(bf, af, x3, t(1:length(x3)));
 
 figure;
 subplot(3,1,1)
 plot(t, m1);
 title('m1');
 xlabel('t');
-ylabel('m1');
 subplot(3,1,2)
 plot(t, m2);
 title('m2');
 xlabel('t');
-ylabel('m2');
 subplot(3,1,3)
 plot(t, m3);
 title('m3');
 xlabel('t');
-ylabel('m3');
 
-saveas(gcf, "plots/P4_6_g_out.png")
+saveas(gcf, "./P4_6_g.png")
 close
-
-
-% _ . .   D
-% . . .   S
-% . _ _ . P
-
